b704670cf68692f001da2c4655be23a5042fb1b9
NextFire <git@yuru.moe>
2022-09-23 12:44:58 -0400

    [kitty] ssh alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+
 # VSCode Terminal Shell Integration
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
