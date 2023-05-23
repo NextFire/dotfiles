d6fe712f2d22432dbd6fd2ee9a06400b55af52f6
NextFire <git@yuru.moe>
2023-05-23 22:47:09 +0900

    nanak8s update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,17 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# abbr
+abbr -a venv 'python3 -m venv .venv'
+abbr -a jotta 'rclone mount jotta-crypt: ~/rclone --vfs-cache-mode full --daemon'
+abbr -a mpvtct 'mpv --vo=tct --profile=sw-fast --vo-tct-algo=plain --vo-tct-256=yes --really-quiet'
+abbr -a k kubectl
+abbr -a kt 'kubectl -n kubernetes-dashboard create token admin-user | pbcopy'
+abbr -a kp 'kubectl get pods -A -o wide -w'
+abbr -a kd "kubectl drain --delete-emptydir-data --ignore-daemonsets --pod-selector='app!=csi-attacher,app!=csi-provisioner,app!=longhorn-admission-webhook,app!=longhorn-conversion-webhook,app!=longhorn-driver-deployer'"
+
 # Language
-set -x LANG en_US.UTF-8
+#set -x LANG en_US.UTF-8
 
 # Editor
 set -x EDITOR 'code --wait'
