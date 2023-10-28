55075e6a9a8dd9a3d68be77bd8747248137fb9a4
NextFire <git@yuru.moe>
2023-10-28 17:57:35 -0400

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,13 @@ end
 
 # abbr
 abbr -a venv 'python3 -m venv --upgrade-deps .venv'
-abbr -a jotta 'rclone mount jotta-crypt: ~/rclone --vfs-cache-mode full --daemon'
+abbr -a jotta 'rclone mount jotta-crypt: ~/rclone --vfs-cache-mode writes --daemon'
+abbr -a deck 'rclone mount steamdeck:/run/media/mmcblk0p1 ~/rclone --vfs-cache-mode writes'
 abbr -a mpvtct 'mpv --vo=tct --profile=sw-fast --vo-tct-algo=plain --vo-tct-256=yes --really-quiet'
 abbr -a k kubectl
 abbr -a kt 'kubectl -n kubernetes-dashboard create token admin-user | pbcopy'
 abbr -a kp 'kubectl get pods -A -o wide -w'
-abbr -a kd "kubectl drain --delete-emptydir-data --ignore-daemonsets --pod-selector='app!=csi-attacher,app!=csi-provisioner,app!=longhorn-admission-webhook,app!=longhorn-conversion-webhook,app!=longhorn-driver-deployer'"
+abbr -a kd 'kubectl drain --ignore-daemonsets --delete-emptydir-data' 
 
 # Language
 #set -x LANG en_US.UTF-8
