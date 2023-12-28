d59bf76b2879c0e3bb49e905d4742fdfd3e2d02e
NextFire <git@yuru.moe>
2023-12-28 11:06:35 +0100

    up

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,18 +5,22 @@ end
 # abbr
 abbr -a venv 'python3 -m venv --upgrade-deps .venv'
 abbr -a jotta 'rclone mount jotta-crypt: ~/rclone --vfs-cache-mode writes --daemon'
-abbr -a deck 'rclone mount steamdeck:/run/media/mmcblk0p1 ~/rclone --vfs-cache-mode writes'
+abbr -a sd 'rclone mount steamdeck:/run/media/mmcblk0p1 ~/rclone --vfs-cache-mode writes'
 abbr -a mpvtct 'mpv --vo=tct --profile=sw-fast --vo-tct-algo=plain --vo-tct-256=yes --really-quiet'
 abbr -a k kubectl
 abbr -a kt 'kubectl -n kubernetes-dashboard create token admin-user | pbcopy'
 abbr -a kp 'kubectl get pods -A -o wide -w'
+abbr -a kn 'kubectl get nodes -o wide -w'
 abbr -a kd 'kubectl drain --ignore-daemonsets --delete-emptydir-data' 
 
 # Language
-#set -x LANG en_US.UTF-8
-
+set -x LANG en_US.UTF-8
 # Editor
 set -x EDITOR 'code --wait'
-
 # Java
 #set -x JAVA_HOME (/usr/libexec/java_home)
+# Go
+set -x GOPATH ~/.go
+
+# Rancher Desktop
+fish_add_path -aP ~/.rd/bin
